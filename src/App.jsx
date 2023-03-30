9530a250c388d021d727fffaf8825d938e2f03f1
Doit-Goal <swspolice@naver.com>
2023-03-30 11:50:56 +0900

    tailwind 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import "./App.css";
+
 function App() {
   return (
     <div>
-      <h1>Hello, React!</h1>
-      <h2>My Name is sws</h2>
-      <h3>제로콜라</h3>
-      <h4>🍔༼ つ ◕_◕ ༽つ🍔</h4>
+      <h1 className="text-3xl font-bold underline">Hello, React!</h1>
+      <h2 className="inline-block bg-color-aqua p-12 b-r-12 m-r-12">
+        My Name is sws
+      </h2>
+      <h3 className="inline-block bg-color-violet p-12 b-r-12 m-r-12">
+        제로콜라
+      </h3>
     </div>
   );
 }
